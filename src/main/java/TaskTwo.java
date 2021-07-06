da32b1f096b94e719b963dd00441a0e2fc412d3f
Alex G <ulledar@ua.fm>
2021-07-06 11:49:00 +0300

    Upd TaskTwo resolve comments tasks

--- src/main/java/TaskTwo.java
+++ src/main/java/TaskTwo.java
@@ -1,9 +1,9 @@
-import java.util.Scanner;
+import Utils.InputReader;
 
 public class TaskTwo {
 
     public static void main(String[] args) {
-        String str = inputString();
+        String str = getUserInput();
 
         String[] splittedString = splitString(str);
         String unfoldedString = getUnfoldString(splittedString);
@@ -12,49 +12,48 @@ public class TaskTwo {
         char[] chars = toCharArray(str);
         char[] unfoldedChars = unfoldedChars(chars);
         String unfoldStr = new String(unfoldedChars);
-//        String unfoldStr = String.valueOf(unfoldedChars);
         System.out.println(unfoldStr);
     }
 
-    public static char[] toCharArray(String inputString) {
-        return inputString.toCharArray();
+    public static char[] toCharArray(String getUserInput) {
+        return getUserInput.toCharArray();
     }
 
     public static char[] unfoldedChars(char[] chars) {
         char[] unfoldedCharsArray = new char[chars.length];
-        int a = chars.length-1;
+        int tempValue = chars.length-1;
 
         for (char aChar : chars) {
-            unfoldedCharsArray[a] = aChar;
-            a--;
+            unfoldedCharsArray[tempValue] = aChar;
+            tempValue--;
         }
         return unfoldedCharsArray;
     }
 
     public static String getUnfoldString(String[] array) {
         String[] reverseStrIntoArray = new String[array.length];
-        int a = array.length-1;
+        int tempValue = array.length-1;
         String delimiter = "";
 
         for (String s : array) {
-            reverseStrIntoArray[a] = s;
-            a--;
+            reverseStrIntoArray[tempValue] = s;
+            tempValue--;
         }
 
         return String.join(delimiter, reverseStrIntoArray);
     }
 
-    public static String[] splitString(String inputString) {
-        return inputString.split("");
+    public static String[] splitString(String getUserInput) {
+        return getUserInput.split("");
     }
 
-    public static String inputString() {
+    public static String getUserInput() {
         System.out.println("Hi! Enter any sentence and I will unfold it! But not only numbers ;)");
-        String str = new Scanner(System.in).nextLine();
+        String str = InputReader.readString();
 
         if (str.length() > 100 || str.length() <= 0) {
             System.out.println("Please enter more than 0 and less than 100 symbols");
-            inputString();
+            getUserInput();
         }
 
         return str;
