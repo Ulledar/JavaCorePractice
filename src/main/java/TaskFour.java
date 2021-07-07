e95c324e3ec1bd6590975013d3134caf10fa27f9
Alex G <ulledar@ua.fm>
2021-07-07 15:55:56 +0300

    Task Four upd. decimals added

--- src/main/java/TaskFour.java
+++ src/main/java/TaskFour.java
@@ -3,12 +3,20 @@ import Utils.InputReader;
 public class TaskFour {
 
     public static void main(String[] args) {
-        int inputHours = InputReader.readInt();
+        double inputHours = InputReader.readDouble();
         System.out.println(getSecondsFromHours(inputHours));
     }
 
-    public static int getSecondsFromHours(int hours) {
-        return hours * 3600;
-    }
+    public static int getSecondsFromHours(double time) {
+        String[] timeStringArray = String.valueOf(time).split("\\.");
+        String[] decimals = timeStringArray[1].split("");
+        int hoursInSeconds = Integer.parseInt(timeStringArray[0]) * 3600;
+        int minutesInSeconds = Integer.parseInt(decimals[0]) * 360;
 
+        if (decimals.length > 1) {
+            minutesInSeconds = (Integer.parseInt(decimals[0]) * 360) + (Integer.parseInt(decimals[1]) * 36);
+        }
+
+        return hoursInSeconds + minutesInSeconds;
+    }
 }
