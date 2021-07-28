1f33be58c2a0f3715e17759b03fe271b8f7e72b9
Alex G <ulledar@ua.fm>
2021-07-28 10:59:35 +0300

    Stream API method added to FilterArray task class

--- src/main/java/FilterArray.java
+++ src/main/java/FilterArray.java
@@ -1,17 +1,23 @@
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class FilterArray {
 
     public static void main(String[] args) {
         Object[] array = {"2123", "asbb", 7, 54, "31", "asadd", 16, 99999, "15", 31, "12312"};
 
-        for (Integer listElement : filterListFromStrings(array)) {
-            System.out.println(listElement);
+        for (Integer listElement : filterArrayFromStrings(array)) {
+            System.out.print(listElement+", ");
+        }
+
+        System.out.println();
+
+        for (Object listElement : filterArrayFromStringsByStreamApi(array)) {
+            System.out.print(listElement+", ");
         }
     }
 
-    public static List<Integer> filterListFromStrings(Object[] ar) {
+    public static List<Integer> filterArrayFromStrings(Object[] ar) {
         List<Integer> filteredList = new LinkedList<>();
 
         for (Object o : ar) {
@@ -21,5 +27,11 @@ public class FilterArray {
         }
 
         return filteredList;
+    }
+
+    public static List<Object> filterArrayFromStringsByStreamApi(Object[] ar) {
+        return Arrays.stream(ar)
+                .filter(s -> s.getClass() == Integer.class)
+                .collect(Collectors.toCollection(ArrayList::new));
     }
 }
